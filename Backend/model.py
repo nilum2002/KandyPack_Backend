79b8205a73d0a56eea5a2250522f62cd8fbfcd95
Nilum <sachithyanilum@gmail.com>
2025-09-16 14:57:49 +0530

    data base updated

--- Backend/model.py
+++ Backend/model.py
@@ -2,34 +2,25 @@ from sqlalchemy import Column, Boolean, Integer, String, ForeignKey, event, Date
 from database import Base
 from datetime import datetime, timezone
 from sqlalchemy.orm import relationship, sessionmaker
+import uuid 
 
 
-
+# generate uuid 
+def generate_uuid():
+    return str(uuid.uuid4())
 
 class Customers(Base):
     __tablename__ = "customers"
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    customer_id = Column(String(20), unique = True, index = True)
+    # id = Column(Integer, primary_key=True, autoincrement=True)
+    customer_id = Column(String(36), primary_key=True, index = True, default=generate_uuid())
     customer_name = Column(String(100))
     phone_number = Column(String(20))
     address = Column(String(200))
 
-
-# auto generating the customer id 
-@event.listens_for(Customers,"after_insert")
-def customer_id_generate(mapper, connection, target):
-    new_id = f"CID_{target.id:05d}"
-    connection.execute(
-        Customers.__table__.update()
-        .where(Customers.id == target.id)
-        .values(customer_id = new_id)
-    )
-    
-
 class Orders(Base):
     __tablename__ = "orders"
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    Order_id = Column(String(20), unique = True, index = True)
+    # id = Column(Integer, primary_key=True, autoincrement=True)
+    Order_id = Column(String(36), primary_key=True, index = True, default= generate_uuid)
     customer_id = Column(String(20), ForeignKey("customers.customer_id"))
     order_date = Column(DateTime, default = lambda : datetime.now(timezone.utc))
     deliver_address = Column(String(200))
@@ -37,15 +28,6 @@ class Orders(Base):
     Deliver_city = Column(String(50))
     full_price = Column(Integer)
 
-# auto increment order id 
-@event.listens_for(Orders,"after_insert")
-def order_id_generate(mapper, connection, target):
-    new_id = f"OID_{target.id:05d}"
-    connection.execute(
-        Orders.__table__.update()
-        .where(Orders.id == target.id)
-        .values(Order_id = new_id)
-    )
 
     
 
