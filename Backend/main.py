89e319916478a6940038d70d8434bce5d3d0c237
Nilum <sachithyanilum@gmail.com>
2025-09-16 14:58:26 +0530

    Add new Pydantic models for store, routes, products, and order items in main.py and model.py

--- Backend/main.py
+++ Backend/main.py
@@ -1,7 +1,6 @@
 from pydantic import BaseModel
 from typing import Annotated
 from database import engine, Session_local
-from sqlalchemy.orm import Session
 import model
 
 
@@ -27,7 +26,97 @@ class orderBase(BaseModel):
     Deliver_city : str 
     full_price  : int 
 
+class storeBase(BaseModel):
+    store_id : str
+    telephone_number : str 
+    name : str 
+    address : str 
+    near_railway_station : str 
+    contact_person : str 
 
+class routeBase(BaseModel):
+    route_id : str 
+    route_name : str 
+    Order_id : str 
+    distance : str 
+    store_id : str 
+    start_city : str 
+    end_city : str 
+
+class route_cityBase(BaseModel):
+    city : str 
+    route_id : str
+
+class productBase(BaseModel):
+    product_type_id : str 
+    space_consumption_rate : int
+
+class order_itemsBase(BaseModel):
+    item_id : str
+    quantity : int
+    store_id : str 
+    product_type_id : str 
+    item_price : float
+    Order_id : str 
+
+class CityBase(BaseModel):
+    city_id :str 
+    city_name : str
+    province : str 
+    station_id : str  
+
+class RailwaystationBase(BaseModel):
+    station_id : str
+    station_name : str 
+    city_id : str 
+
+class TrainBase(BaseModel):
+    train_id : str 
+    train_name : str 
+    capacity : int 
+
+class Train_SchedulesBase(BaseModel):
+    scheduled_id : str 
+    date : str 
+    scheduled_date : str 
+    departure_time : str 
+    arrival_time : str 
+    station_id : str 
+    train_id : str 
+    status : str 
+
+class RailwayAllocationBase(BaseModel):
+    allocation_id : str 
+    shipment_date : str 
+    status : str 
+    scheduled_id : str 
+
+class TruckBase(BaseModel):
+    truck_id : str 
+    capacity : int
+    license_num : str 
+    is_active : bool
+
+class AssistantBase(BaseModel):
+    assistant_id : str 
+    name : str 
+    weekly_working_hours : int 
+
+class Truck_SchedulesBase(BaseModel):
+    scheduled_id : str 
+    date : str 
+    scheduled_date : str 
+    departure_time : str 
+    duration : int 
+    route_id : str 
+    truck_id : str 
+    assistant_id : str
+    status : str 
+
+class Truck_allocationBase(BaseModel):
+    allocation_id : str 
+    shipment_date : str 
+    scheduled_id : str 
 
 def get_db():
     db = Session_local()
--- Backend/model.py
+++ Backend/model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Boolean, Integer, String, ForeignKey, event, DateTime
+from sqlalchemy import Column, Boolean, Integer, String, ForeignKey, event, DateTime, Float, Date, Time
 from database import Base
 from datetime import datetime, timezone
 from sqlalchemy.orm import relationship, sessionmaker
@@ -12,10 +12,11 @@ def generate_uuid():
 class Customers(Base):
     __tablename__ = "customers"
     # id = Column(Integer, primary_key=True, autoincrement=True)
-    customer_id = Column(String(36), primary_key=True, index = True, default=generate_uuid())
+    customer_id = Column(String(36), primary_key=True, index = True, default=generate_uuid)
     customer_name = Column(String(100))
     phone_number = Column(String(20))
     address = Column(String(200))
+
 
 class Orders(Base):
     __tablename__ = "orders"
@@ -28,8 +29,121 @@ class Orders(Base):
     Deliver_city = Column(String(50))
     full_price = Column(Integer)
 
+class Store(Base):
+    __tablename__ = "store"
+    store_id = Column(String(36),primary_key=True,index=True, default= generate_uuid )
+    telephone_number = Column(String(15))
+    name = Column(String(20))
+    address = Column(String(255))
+    near_railway_station = Column(String(100))
+    contact_person = Column(String(100))
 
+
+class Routes(Base):
+    __tablename__ = "routes"
+    route_id = Column(String(20), primary_key= True,index=True)
+    route_name = Column(String(20))
+    Order_id = Column(String(36), ForeignKey("orders.Order_id"))
+    distance = Column(Integer)
+    store_id = Column(String(36), ForeignKey("store.store_id"))
+    start_city = Column(String(50))
+    end_city = Column(String(50))
+
+
+class Rout_cities(Base):
+    __tablename__ = "route_cities"
+    city = Column(String(50), primary_key=  True)
+    route_id = Column(String(20),ForeignKey("routes.route_id"))
+
+class Product(Base):
+    __tablename__ = "products"
+    product_type_id = Column(String(100), primary_key= True, index= True)
+    space_consumption_rate = Column(Integer)
+
+class Order_items(Base):
+    __tablename__ = "order_items"
+    item_id = Column(String(36), primary_key= True, index=True, default= generate_uuid)
+    quantity = Column(Integer)
+    store_id = Column(String(36), ForeignKey("store.store_id"))
+    product_type_id = Column(String(100), ForeignKey("products.product_type_id"))
+    item_price = Column(Float)
+    Order_id = Column(String(36), ForeignKey("orders.Order_id"))
+
+
+class City(Base):
+    __tablename__ = "city"
+    city_id = Column(String(50), primary_key= True, index=True)
+    city_name = Column(String(100))
+    province = Column(String(100))
+    station_id = Column(String(50), ForeignKey("railway_stations.station_name"))
+
+
+class RailwayStations(Base):
+    __tablename__ = "railway_stations"
+    station_id = Column(String(50), primary_key= True, index=True)
+    station_name = Column(String(100), primary_key= True , index= True )
+    city_id = Column(String(50), ForeignKey("city.city_id"))
+
+class Train(Base):
+    __tablename__ = "trains"
+    train_id = Column(String(50), primary_key= True, index=True)
+    train_name = Column(String(100))
+    capacity = Column(Integer)
+
+class Train_Schedules(Base):
+    __tablename__ = "train_schedules"
+    scheduled_id = Column(String(36), primary_key= True, index= True, default= generate_uuid)
+    date = Column(DateTime, default = lambda : datetime.now(timezone.utc))
+    scheduled_date = Column(Date)
+    departure_time = Column(Time)
+    arrival_time = Column(Time)
+    station_id = Column(String(50), ForeignKey("railway_stations.station_id"))
+    train_id = Column(String(50), ForeignKey("trains.train_id"))
+    status = Column(String(50))
+
+
+class RailAllocations(Base):
+    __tablename__ = "rail_allocations"
+    allocation_id = Column(String(36), primary_key=True, index= True, default=generate_uuid)
+    shipment_date = Column(Date)
+    status = Column(String(20))
+    scheduled_id = Column(String(36), ForeignKey("train_schedules.scheduled_id"))
+
+class Trucks(Base):
+    __tablename__ = "trucks"
+    truck_id = Column(String(36), primary_key=True, index=True, default= generate_uuid)
+    capacity = Column(Integer)
+    license_num = Column(String(50))
+    is_active = Column(Boolean)
+
+class Assistance(Base):
+    __tablename__ = "assistant"
+    assistant_id = Column(String(36), primary_key=  True ,index = True, default= generate_uuid)
+    name = Column(String(100))
+    weekly_working_hours = Column(Integer)
     
+class Truck_Schedules(Base):
+    __tablename__ = "truck_schedules"
+    scheduled_id = Column(String(36), primary_key= True, index= True, default= generate_uuid)
+    date = Column(DateTime, default = lambda : datetime.now(timezone.utc))
+    scheduled_date = Column(Date)
+    departure_time = Column(Time)
+    duration = Column(Integer)
+    route_id = Column(String(20), ForeignKey("routes.route_id"))
+    truck_id = Column(String(36),primary_key= "trucks.truck_id")
+    assistant_id = Column(String(36), ForeignKey("assistant.assistant_id"))
+    status = Column(String(50))
+
+
+class Truck_allocations(Base):
+    __tablename__ = "truck_allocation"
+    allocation_id = Column(String(36), primary_key=True, index=True, default=generate_uuid)
+    shipment_date = Column(Date)
+    scheduled_id = Column(String(36), ForeignKey("truck_schedules.scheduled_id"))
+
+
+
+
 
 
 
