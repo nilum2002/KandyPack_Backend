1654e2e2f20e78512ced4c15e25ff1292120a45e
Nilum <sachithyanilum@gmail.com>
2025-10-09 20:37:18 +0530

    Updated

--- Backend/app/api/__init__.py
+++ Backend/app/api/__init__.py
@@ -6,10 +6,16 @@ from app.api.customers import router as customer_router
 from app.api.users import router as user_router
 from app.api.cities import router as city_router
 from app.api.railway_stations import router as railway_stations
+from app.api.stores import router as stores
+
+
+
+
 api_router = APIRouter()
 api_router.include_router(customer_router)
 api_router.include_router(user_router)
 api_router.include_router(city_router)
 api_router.include_router(railway_stations)
+api_router.include_router(stores)
 
 
